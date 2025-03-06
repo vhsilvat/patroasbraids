46b5f57dbde5430653140211061bd7c898020f1b
Victor Hugo <vhsilvat@gmail.com>
2025-03-06 20:37:49 -0300

    Adiciona import do useEffect no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import { useAuth } from './contexts/AuthContext'
